873db15c1239e1f5b8b413dc7c84f92bb2ec144a
julgrahn <alexander.grahn@hotmail.se>
2020-07-07 01:10:04 +0200

    capture frames every 1 ms, print amount of frames after execution

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import cv2, time
 
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+a = 1
+
 while True:
+    a = a+1
     check, frame = video.read()
 
     print(check)
     print(frame)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)          #convert into gray image
-    time.sleep(3)
+    #time.sleep(3)
     cv2.imshow("Capturing", gray)
 
-    keyPress = cv2.waitKey(1000)
+    keyPress = cv2.waitKey(1)
     if keyPress == ord('q'):
         break
 
+print("Frames: ", a)
 video.release()
 cv2.destroyAllWindows()
